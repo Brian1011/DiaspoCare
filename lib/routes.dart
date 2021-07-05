22bc6deef5a325b7d734c46ad31ccc056a34d26d
Brian Mutinda <mutindabrian26@gmail.com>
2021-07-05 22:16:49 +0300

    initial commit

---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:diaspo_care/screens/splash_screen.dart';
+
+class RouteConfig {
+  static final splash = "/";
+}
+
+var routes = {
+  RouteConfig.splash: (context) => SplashScreen(),
+};
