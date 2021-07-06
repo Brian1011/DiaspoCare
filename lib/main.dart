25bed94626f611bd1032ef0ee79de58d0dd4e3f9
Milai <milai@solutech.co.ke>
2021-07-06 15:01:36 +0300

    merge fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:diaspo_care/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
