d276658158d99f9ee827373dc9fb34dc0ac12844
DavidMilai <davidmilai67@gmail.com>
2021-07-12 14:45:49 +0300

    credit card/Mpesa payment method

--- lib/screens/pharmacist_screens/add_phar_bank_details.dart
+++ lib/screens/pharmacist_screens/add_phar_bank_details.dart
@@ -1,7 +1,10 @@
 import 'package:diaspo_care/widgets/centered_button.dart';
 import 'package:diaspo_care/widgets/custom_appbar.dart';
+import 'package:diaspo_care/widgets/underlined_textfield.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_form.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
 
 import '../../routes.dart';
 
@@ -11,8 +14,14 @@ class AddPharBankDetails extends StatefulWidget {
 }
 
 class _AddPharBankDetailsState extends State<AddPharBankDetails> {
-  int selectedRadioTile;
+  int selectedRadioTile = 0;
   double spacing = 10;
+  String cardNumber, expiryDate, cardHolderName, cvvCode;
+  final GlobalKey<FormState> cardFormKey = GlobalKey<FormState>();
+  TextEditingController mpesaNameTextEditingController =
+      TextEditingController();
+  TextEditingController mpesaNumberTextEditingController =
+      TextEditingController();
 
   @override
   void initState() {
@@ -33,7 +42,6 @@ class _AddPharBankDetailsState extends State<AddPharBankDetails> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -59,7 +67,7 @@ class _AddPharBankDetailsState extends State<AddPharBankDetails> {
                     RadioListTile(
                       value: 1,
                       groupValue: selectedRadioTile,
-                      title: Text("Equity Bank"),
+                      title: Text("Credit Card"),
                       onChanged: (val) {
                         print("Radio Tile pressed $val");
                         setSelectedRadioTile(val);
@@ -69,7 +77,7 @@ class _AddPharBankDetailsState extends State<AddPharBankDetails> {
                       secondary: Container(
                         width: 80,
                         height: 80,
-                        child: Image.asset('assets/equity_logo.png'),
+                        child: Image.asset('assets/credit_card_logo.png'),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(10.0),
@@ -77,6 +85,55 @@ class _AddPharBankDetailsState extends State<AddPharBankDetails> {
                       ),
                       selected: true,
                     ),
+                    selectedRadioTile == 1
+                        ? Card(
+                            child: Column(
+                              children: [
+                                CreditCardForm(
+                                  formKey: cardFormKey, // Required
+                                  onCreditCardModelChange:
+                                      (CreditCardModel data) {}, // Required
+                                  themeColor: Colors.red,
+                                  obscureCvv: true,
+                                  obscureNumber: true,
+                                  cardNumberDecoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Number',
+                                    hintText: 'XXXX XXXX XXXX XXXX',
+                                  ),
+                                  expiryDateDecoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Expired Date',
+                                    hintText: 'XX/XX',
+                                  ),
+                                  cvvCodeDecoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'CVV',
+                                    hintText: 'XXX',
+                                  ),
+                                  cardHolderDecoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Card Holder',
+                                  ),
+                                  cardNumber: cardNumber, cvvCode: cvvCode,
+                                  cardHolderName: cardHolderName,
+                                  expiryDate: expiryDate,
+                                ),
+                                SizedBox(height: spacing),
+                                CenteredButton(
+                                  size: size,
+                                  onPressed: openBeneficiary,
+                                  child: Text(
+                                    'Save Card',
+                                    style: TextStyle(
+                                        fontSize: 18.0, color: Colors.white),
+                                  ),
+                                ),
+                                SizedBox(height: spacing),
+                              ],
+                            ),
+                          )
+                        : Container(),
                     Divider(
                       color: Colors.black,
                       thickness: 1,
@@ -104,94 +161,50 @@ class _AddPharBankDetailsState extends State<AddPharBankDetails> {
                       ),
                       selected: true,
                     ),
+                    selectedRadioTile == 2
+                        ? Card(
+                            child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Payment Information',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: spacing),
+                                UnderlinedTextField(
+                                    obscureText: false,
+                                    hintText: 'Registered M-Pesa Name',
+                                    controller: mpesaNameTextEditingController),
+                                SizedBox(height: spacing),
+                                UnderlinedTextField(
+                                    obscureText: false,
+                                    hintText: 'M-Pesa Phone Number',
+                                    controller:
+                                        mpesaNumberTextEditingController),
+                                SizedBox(height: spacing),
+                                CenteredButton(
+                                  size: size,
+                                  onPressed: openBeneficiary,
+                                  child: Text(
+                                    'Add Number',
+                                    style: TextStyle(
+                                        fontSize: 18.0, color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ))
+                        : Container(),
                     Divider(
                       color: Colors.black,
                       thickness: 1,
                       indent: 20,
                       endIndent: 20,
                     ),
-                    // RadioListTile(
-                    //   value: 3,
-                    //   groupValue: selectedRadioTile,
-                    //   title: Text("PesaLink"),
-                    //   onChanged: (val) {
-                    //     print("Radio Tile pressed $val");
-                    //     setSelectedRadioTile(val);
-                    //   },
-                    //   activeColor: Colors.blue,
-                    //   //secondary: Image.asset('assets/pesaLink_logo.jpg'),
-                    //   secondary: Container(
-                    //     width: 80,
-                    //     height: 80,
-                    //     child: Image.asset('assets/pesaLink_logo.jpg'),
-                    //     decoration: BoxDecoration(
-                    //       color: Colors.white,
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //     ),
-                    //   ),
-                    //   selected: true,
-                    // ),
-                    // Divider(
-                    //   color: Colors.black,
-                    //   thickness: 1,
-                    //   indent: 20,
-                    //   endIndent: 20,
-                    // ),
-                    // RadioListTile(
-                    //   value: 4,
-                    //   groupValue: selectedRadioTile,
-                    //   title: Text("Stripe"),
-                    //   onChanged: (val) {
-                    //     print("Radio Tile pressed $val");
-                    //     setSelectedRadioTile(val);
-                    //   },
-                    //   activeColor: Colors.blue,
-                    //   //secondary: Image.asset('assets/Stripe_logo.png'),
-                    //   secondary: Container(
-                    //     width: 80,
-                    //     height: 80,
-                    //     child: Image.asset('assets/Stripe_logo.png'),
-                    //     decoration: BoxDecoration(
-                    //       color: Colors.white,
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //     ),
-                    //   ),
-                    //   selected: true,
-                    // ),
-                    // Divider(
-                    //   color: Colors.black,
-                    //   thickness: 1,
-                    //   indent: 20,
-                    //   endIndent: 20,
-                    // ),
-                    // RadioListTile(
-                    //   value: 5,
-                    //   groupValue: selectedRadioTile,
-                    //   title: Text("Paypal"),
-                    //   onChanged: (val) {
-                    //     print("Radio Tile pressed $val");
-                    //     setSelectedRadioTile(val);
-                    //   },
-                    //   activeColor: Colors.blue,
-                    //   //secondary: Image.asset('assets/paypal_logo.png'),
-                    //   secondary: Container(
-                    //     width: 80,
-                    //     height: 80,
-                    //     child: Image.asset('assets/paypal_logo.png'),
-                    //     decoration: BoxDecoration(
-                    //       color: Colors.white,
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //     ),
-                    //   ),
-                    //   selected: true,
-                    // ),
-                    // Divider(
-                    //   color: Colors.black,
-                    //   thickness: 1,
-                    //   indent: 20,
-                    //   endIndent: 20,
-                    // ),
-                    SizedBox(height: spacing * 10),
+                    SizedBox(height: 10),
                     CenteredButton(
                       size: size,
                       onPressed: openBeneficiary,
